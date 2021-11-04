952c9bea25d521214b4faeae03c4c8bc117c030e
wwpeng520 <wwpeng520@gmail.com>
2021-11-04 13:40:28 +0800

    💥 feat: more

--- convert.py
+++ convert.py
@@ -1,10 +1,10 @@
 import pdfkit
 options = {
     'page-size': 'A4',
-    'margin-top': '0.75in',
-    'margin-right': '0.75in',
-    'margin-bottom': '0.75in',
-    'margin-left': '0.75in',
+    'margin-top': '0.25in',
+    'margin-right': '0.25in',
+    'margin-bottom': '0.25in',
+    'margin-left': '0.25in',
     'encoding': "UTF-8",
     'custom-header': [
         ('Accept-Encoding', 'gzip')
@@ -13,7 +13,8 @@ options = {
         ('cookie-name1', 'cookie-value1'),
         ('cookie-name2', 'cookie-value2'),
     ],
-    'no-outline': None
+    'no-outline': None,
+    "enable-local-file-access": None
 }
 
-pdfkit.from_file('first.html', '简历.pdf')
+pdfkit.from_file('first.html', '简历.pdf', options=options)
